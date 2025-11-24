a9b9743ad9372e32d88e501ee871c3d5edb3dd19
Shuchita Taneja <shtaneja@microsoft.com>
2025-11-24 11:46:59 +1100

    kube login update

--- 3-aks/main.tf
+++ 3-aks/main.tf
@@ -106,22 +106,9 @@ data "azurerm_kubernetes_cluster" "credentials" {
 
 provider "kubernetes" {
   host                   = data.azurerm_kubernetes_cluster.credentials.kube_config[0].host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].cluster_ca_certificate)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "az"
-    args = [
-      "aks",
-      "get-credentials",
-      "--resource-group",
-      azurerm_resource_group.aks.name,
-      "--name",
-      "aks-${var.name}",
-      "--format",
-      "exec",
-    ]
-  }
 }
 
 # =====================================================
